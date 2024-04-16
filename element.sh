d7a4759841535f0b94f3a7571427e540fed1a220
FREDY AKTOSA <workmailAkto@gmail.com>
2024-04-16 03:38:55 +0000

    fix: all query is done

--- element.sh
+++ element.sh
@@ -2,7 +2,6 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-echo -e "$1"
 if [[ $1 ]]
 then
   if [[ ! $1 =~ ^[0-9]+$ ]]
@@ -11,13 +10,24 @@ then
     ATOMIC_NUMBER=$(echo "$ELEMENT_PRESENT" | cut -d '|' -f 1)
     SYMBOL=$(echo "$ELEMENT_PRESENT" | cut -d '|' -f 2)
     NAME=$(echo "$ELEMENT_PRESENT" | cut -d '|' -f 3)
-    echo -e "$ATOMIC_NUMBER, $SYMBOL, $NAME"
   else
     ELEMENT_PRESENT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1")
     ATOMIC_NUMBER=$(echo "$ELEMENT_PRESENT" | cut -d '|' -f 1)
     SYMBOL=$(echo "$ELEMENT_PRESENT" | cut -d '|' -f 2)
     NAME=$(echo "$ELEMENT_PRESENT" | cut -d '|' -f 3)
-    echo -e "$ATOMIC_NUMBER, $SYMBOL, $NAME"
+  fi
+
+  if [[ -z  $ELEMENT_PRESENT ]]
+  then
+   echo "I could not find that element in the database."
+  else
+    PROPERTIES=$($PSQL "SELECT * FROM properties JOIN types USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER")
+    ATOMIC_MASS=$(echo "$PROPERTIES" | cut -d '|' -f 3)
+    MELT_POINT=$(echo "$PROPERTIES" | cut -d '|' -f 4)
+    BOIL_POINT=$(echo "$PROPERTIES" | cut -d '|' -f 5)
+    TYPE=$(echo "$PROPERTIES" | cut -d '|' -f 6)
+
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELT_POINT celsius and a boiling point of $BOIL_POINT celsius."
   fi
 else
   echo "Please provide an element as an argument."
